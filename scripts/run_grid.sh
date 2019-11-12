9e732d7a1c319ce217f258549552f383313d6f06
Jason Miller (jrm0122@mix.wvu.edu) -- demclaughlin Group <jrm0122@srih0001.hpc.wvu.edu>
2019-11-12 16:12:01 -0500

    First version to work on grid.

--- scripts/run_grid.sh
+++ scripts/run_grid.sh
@@ -11,18 +11,28 @@
 #PBS -m n   ## no email
 ###PBS -M < your email here >
 #PBS -N LNC1
+
 echo -n "START AT "; date
 module load lang/python/2.7.15_gcc82
 cd /users/jrm0122/RunLncADeep
-source venv/bin/activate
+source venv/bin/activate     # virtual environment with paths to python libraries
 cd /scratch/jrm0122/RunLncADeep/Run.001
 echo My Task $PBS_ARRAYID
-MYFILE=min200.lncRNA.fasta
-MYFILE=subset1.fasta     # for testing
-MYSIZE=5
-MYSTART=( (${PBS_ARRAYID}-1) * $MYSIZE )
-MYSUBSET=my.${PBS_ARRAYID}.fasta
-MYOUTPUT=my.${PBS_ARRAYID}.out
+if [[ "x${PBS_ARRAYID}x" == "xx" ]] ; then
+    echo Please run this as a task array e.g. qsub -t 1-10
+    exit 1
+fi
+
+MYFILE=min200.lncRNA.fasta   # gencode lncRNA len>=200
+MYFILE=subset1.fasta     # subset for testing
+
+MYSIZE=500     # sequences per grid job
+MYSIZE=5     # tiny size for testing
+
+MYSTART=$(( ${PBS_ARRAYID}-1 ))   # first sequence given array ID
+MYSTART=$(( ${MYSTART} * ${MYSIZE} ))   # first sequence given array ID
+MYSUBSET=my.${PBS_ARRAYID}.fasta           # input fasta for one job
+MYOUTPUT=my.${PBS_ARRAYID}.out             # output directory for one job
 echo MYSTART $MYSTART
 echo MYSUBSET $MYSUBSET
 echo MYOUTPUT $MYOUTPUT
@@ -30,9 +40,13 @@ echo MYOUTPUT $MYOUTPUT
 echo Extract my group of sequences
 echo "python fasta_extractor.py $MYFILE $MYSTART $MYSIZE $MYSUBSET"
      python fasta_extractor.py $MYFILE $MYSTART $MYSIZE $MYSUBSET
+echo -n $?; echo " exit status"
+ls -l
 
 echo Process the subset
 echo python LncADeep.py --MODE lncRNA --fasta $MYSUBSET --species human --model full --out $MYOUTPUT
      python LncADeep.py --MODE lncRNA --fasta $MYSUBSET --species human --model full --out $MYOUTPUT
+echo -n $?; echo " exit status"
+ls -l
 
 echo -n "FINISH AT ";  date
