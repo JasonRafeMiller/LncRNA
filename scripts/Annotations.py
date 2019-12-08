ce705281969d07734255340dea296c33266bec06
JasonRafeMiller <Jason.Rafe.Miller@gmail.com>
2019-12-08 13:03:12 -0500

    Bug fix. It was printing gene features.

--- scripts/Annotations.py
+++ scripts/Annotations.py
@@ -31,6 +31,10 @@ class Annotations:
         with open(self.infile,"r") as infile, open(self.outfile,"w") as outfile:
             tsvin = csv.reader(infile, delimiter='\t')
             tsvout = csv.writer(outfile, delimiter='\t')
+            outs=('tr_id','feature','strand',
+                  'genomic_start','genomic_end',
+                  'transcriptomic_start','transcriptomic_end')
+            tsvout.writerow(outs)            
             # keep genomic start/end and transcriptomic start/end
             this_feature={'gs':0,'ge':0,'ts':0,'te':0}
             prev_exon={'gs':0,'ge':0,'ts':0,'te':0}
@@ -38,30 +42,33 @@ class Annotations:
                 if self.is_data_line(oneline):
                     comments=oneline[8]
                     tr_id=self.extract_transcript(comments)
-                    feature=oneline[2]
-                    gs=int(oneline[3])
-                    ge=int(oneline[4])
-                    strand=oneline[6]
-                    length=ge-gs+1
-                    if feature=="transcript":
-                        ts=1
-                        te=ts+length-1
-                        this_feature={'gs':gs,'ge':ge,'ts':ts,'te':te}
-                        prev_exon={'gs':0,'ge':0,'ts':0,'te':0}
-                    elif feature=="exon":
-                        ts=prev_exon['te']+1
-                        te=ts+length-1
-                        this_feature={'gs':gs,'ge':ge,'ts':ts,'te':te}
-                        prev_exon={'gs':gs,'ge':ge,'ts':ts,'te':te}
-                    else:
-                        ts=prev_exon['ts']+(gs-prev_exon['gs'])
-                        te=ts+length-1                        
-                        this_feature={'gs':gs,'ge':ge,'ts':ts,'te':te}
-                    outs=(tr_id,feature,strand,
-                          this_feature['gs'],this_feature['ge'],
-                          this_feature['ts'],this_feature['te'])
-                    tsvout.writerow(outs)
-                    
+                    if tr_id:    # genes do not have a transcript id, so ignore them
+                        feature=oneline[2]
+                        gs=int(oneline[3])
+                        ge=int(oneline[4])
+                        strand="pos"
+                        if oneline[6]=="-":
+                            strand="neg"
+                        length=ge-gs+1
+                        if feature=="transcript":
+                            ts=1
+                            te=ts+length-1
+                            this_feature={'gs':gs,'ge':ge,'ts':ts,'te':te}
+                            prev_exon={'gs':0,'ge':0,'ts':0,'te':0}
+                        elif feature=="exon":
+                            ts=prev_exon['te']+1
+                            te=ts+length-1
+                            this_feature={'gs':gs,'ge':ge,'ts':ts,'te':te}
+                            prev_exon={'gs':gs,'ge':ge,'ts':ts,'te':te}
+                        else:
+                            ts=prev_exon['ts']+(gs-prev_exon['gs'])
+                            te=ts+length-1                        
+                            this_feature={'gs':gs,'ge':ge,'ts':ts,'te':te}
+                            outs=(tr_id,feature,strand,
+                                  this_feature['gs'],this_feature['ge'],
+                                  this_feature['ts'],this_feature['te'])
+                            tsvout.writerow(outs)
+                                
                     
 
     def arg_parser():
