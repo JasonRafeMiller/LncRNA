55b4b2a2def4178e6ffbdb3a69fc58a4986c3f86
JasonRafeMiller <Jason.Rafe.Miller@gmail.com>
2019-12-09 19:03:27 -0500

    Intersect parses the FASTA.

--- scripts/Intersect.py
+++ scripts/Intersect.py
@@ -5,9 +5,26 @@ import csv
 
 class Intersection:
     fastafile=""
+    index_to_seqid=[]
+    seqid_to_index={}
 
     def __init__ (self,fasta):
         self.fastafile=fasta
+        self.index_to_seqid=[]
+        self.seqid_to_index={}
+
+    def parse_sequence_ids(self):
+        index = 0
+        with open(self.fastafile,"r") as infile:
+            for oneline in infile:
+                #oneline=oneline.rstrip()
+                if (oneline.startswith(">")):
+                    seqid = oneline[2:]
+                    #print("{}={}".format(index,seqid))
+                    self.index_to_seqid.append(seqid)
+                    self.seqid_to_index[seqid]=index
+                    index=index+1
+
 
     def arg_parser():
         parser = argparse.ArgumentParser(description="List annotations per critical position.")
@@ -21,7 +38,8 @@ if __name__ == '__main__':
     try:
         Intersection.arg_parser()
         it = Intersection(args.fastafile)
-        
+        it.parse_sequence_ids()
+
     except Exception as e:
         print("\nERROR!\n")
         if args.debug:
