ea84ea38dda5522ef9cdcd2302940ca4abfd2f07
JasonRafeMiller <Jason.Rafe.Miller@gmail.com>
2019-12-09 19:19:05 -0500

    Parse annotations

--- scripts/Intersect.py
+++ scripts/Intersect.py
@@ -5,13 +5,17 @@ import csv
 
 class Intersection:
     fastafile=""
+    annofile=""
     index_to_seqid=[]
     seqid_to_index={}
+    annotation_per_transcript={}
 
-    def __init__ (self,fasta):
+    def __init__ (self,fasta,anno):
         self.fastafile=fasta
+        self.annofile=anno
         self.index_to_seqid=[]
         self.seqid_to_index={}
+        self.annotation_per_transcript={}
 
     def parse_sequence_ids(self):
         index = 0
@@ -25,10 +29,27 @@ class Intersection:
                     self.seqid_to_index[seqid]=index
                     index=index+1
 
+    def parse_annotations(self):
+        HEADER=0
+        line=0
+        with open(self.annofile,"r") as infile:
+            tsvin = csv.reader(infile, delimiter='\t')
+            for oneline in tsvin:
+                if line>HEADER:
+                    seqid=oneline[0]
+                    classification=oneline[1]
+                    trstart=int(oneline[5])
+                    trend=int(oneline[6])
+                    one_annot=(classification,trstart,trend)
+                    if seqid not in self.annotation_per_transcript:
+                        self.annotation_per_transcript[seqid]=[]
+                    self.annotation_per_transcript[seqid].append(one_annot)
+                line=line+1
 
     def arg_parser():
         parser = argparse.ArgumentParser(description="List annotations per critical position.")
         parser.add_argument('fastafile', help='fasta input file', type=str)
+        parser.add_argument('annofile', help='annotation input file', type=str)
         parser.add_argument('--debug', help='See tracebacks', action='store_true')
         global args
         args = parser.parse_args()
@@ -37,8 +58,9 @@ class Intersection:
 if __name__ == '__main__':
     try:
         Intersection.arg_parser()
-        it = Intersection(args.fastafile)
+        it = Intersection(args.fastafile,args.annofile)
         it.parse_sequence_ids()
+        it.parse_annotations()
 
     except Exception as e:
         print("\nERROR!\n")
