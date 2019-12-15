b2383401774a5962462f894dfb7f6c0061195cbe
JasonRafeMiller <Jason.Rafe.Miller@gmail.com>
2019-12-15 09:42:46 -0500

    Latest version of grid script.

--- scripts/grid.classify.sh
+++ scripts/grid.classify.sh
@@ -6,17 +6,18 @@
 ####PBS -l nodes=1:ppn=1
 ####PBS -l procs=1
 #PBS -l nodes=1:ppn=1
-#PBS -l walltime=00:10:00
+#PBS -l walltime=00:20:00
 ###PBS -m ae   ## email onabort and exit
 #PBS -m n   ## no email
 ###PBS -M < your email here >
-#PBS -N LNC100
+#PBS -N LNC108
 
 echo -n "START AT "; date
 module load lang/python/2.7.15_gcc82
 cd /users/jrm0122/RunLncADeep
 source venv/bin/activate     # virtual environment with paths to python libraries
-cd /scratch/jrm0122/RunLncADeep/Run.100
+cd /scratch/jrm0122/RunLncADeep/Run.108
+
 echo My Task $PBS_ARRAYID
 if [[ "x${PBS_ARRAYID}x" == "xx" ]] ; then
     echo Please run this as a task array e.g. qsub -t 1-10
@@ -42,7 +43,7 @@ echo -n $?; echo " exit status LncADeep"
 ls -l
 echo CLEANUP
 mv -nv ${MYOUTPUT}_LncADeep_lncRNA_results/${MYOUTPUT}_LncADeep.results ${MYOUTPUT}
-#rm -rf ${MYOUTPUT}_LncADeep_lncRNA_results
+rm -rf ${MYOUTPUT}_LncADeep_lncRNA_results
 ls -l
 
 echo -n "FINISH AT ";  date
